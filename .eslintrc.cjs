3daa23bb9faebdb32cc84c4e89afbd6178fc6013
hckang80 <hckang80@gmail.com>
2023-11-02 18:15:44 +0900

    style: 포맷 룰 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,55 @@
 module.exports = {
-	root: true,
-	extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:svelte/recommended",
-        "prettier",
-        "plugin:storybook/recommended"
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'eslint-plugin-import',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'require-await': 'warn',
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'generator-star-spacing': 'off',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
     ],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
-};
+    'no-lonely-if': 'error',
+    curly: ['error', 'all'],
+    'dot-notation': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'no-useless-rename': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+}
