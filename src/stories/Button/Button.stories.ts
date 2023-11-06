ac5636d0c9227090988abd7147faed735891b386
hckang80 <hckang80@gmail.com>
2023-11-06 12:44:22 +0900

    fix: 경로 수정

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/svelte'
 
 import Button from './Button.svelte'
-import { buttonVariant } from './button'
+import { buttonVariant } from './type'
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 const meta = {
