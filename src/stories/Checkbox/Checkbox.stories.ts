b747f25cdcc15dd7ce46ae5517d59886ad926c8c
hckang80 <hckang80@gmail.com>
2023-11-10 17:44:53 +0900

    feat: 체크박스 컴포넌트 추가

---
+++ src/stories/Checkbox/Checkbox.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/svelte'
+
+import Checkbox from './Checkbox.svelte'
+
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
+const meta = {
+  title: 'Example/Selection/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+  argTypes: {
+    checked: {
+      description: 'bind:{property} 형태로 전달하면 양방향으로 바인딩됩니다.',
+    },
+    size: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} satisfies Meta<Checkbox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Basic: Story = {
+  args: {
+    checked: false,
+    rounded: false,
+  },
+}
+
+export const Rounded: Story = {
+  args: {
+    rounded: true,
+  },
+}
