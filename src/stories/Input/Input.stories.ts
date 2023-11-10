e3069e5e85099571a749dab49a729e9a33ef0e4f
hckang80 <hckang80@gmail.com>
2023-11-10 15:25:08 +0900

    feat: 인풋 컴포넌트 생성

---
+++ src/stories/Input/Input.stories.ts
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/svelte'
+
+import Input from './Input.svelte'
+
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
+const meta = {
+  title: 'Example/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {
+    value: {
+      description: 'bind:value로 전달하면 양방향으로 바인딩됩니다.',
+    },
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+    clearable: {
+      description: '요소 안의 X버튼을 클릭하여 value를 제거합니다.',
+    },
+  },
+} satisfies Meta<Input>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Basic: Story = {
+  args: {
+    label: '라벨',
+    disabled: false,
+    placeholder: '플레이스 홀더',
+    value: '입력한 정보',
+    description: '설명글',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    value: '입력한 정보',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    errorMessage: '에러 힌트',
+  },
+}
---
+++ src/stories/Input/Input.svelte
@@ -0,0 +1,197 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  export let id = ''
+  export let disabled = false
+  export let value = ''
+  export let errorMessage = ''
+  export let label = ''
+  export let clearable = true
+  export let description = ''
+
+  const dispatcher = createEventDispatcher<{
+    input: Event & {
+      currentTarget: EventTarget & HTMLInputElement
+    }
+  }>()
+
+  $: hasError = !!errorMessage
+
+  let inputEl: HTMLInputElement
+
+  const randomId = Math.random().toString(36).slice(2)
+  const _id = id || randomId
+
+  const clear = () => {
+    value = ''
+    inputEl.focus()
+  }
+
+  $: message = errorMessage || description
+</script>
+
+<div class="input-wrap">
+  {#if label}
+    <label class="input-wrap__label" for={_id}>{label}</label>
+  {/if}
+  <div class="input" class:input--error={hasError} class:input--disabled={disabled}>
+    <span class="input__prefix">
+      <input
+        bind:this={inputEl}
+        bind:value
+        {...$$restProps}
+        class="input__el"
+        id={_id}
+        {disabled}
+        on:input={(event) => dispatcher('input', event)}
+        on:focus
+        on:blur
+      />
+      {#if clearable && value && !disabled}
+        <button type="button" class="input__clear" on:click={clear}>
+          <span class="input__clear-inner">clear</span>
+        </button>
+      {/if}
+    </span>
+    <span class="input__suffix"><slot /></span>
+  </div>
+</div>
+
+{#if !!message}
+  <p class="message" class:message--error={hasError}>{message}</p>
+{/if}
+
+<style lang="scss">
+  .input-wrap {
+    &__label {
+      display: block;
+      margin: 0 0 12px;
+      font-size: 13px;
+      line-height: 1.3;
+      color: var(--dd__gray--500);
+    }
+  }
+
+  .input {
+    --padding: 16px;
+    display: flex;
+    align-items: center;
+    padding-inline: var(--padding);
+    outline: 1px solid rgba(#b0b0ba, 0.5);
+    border-radius: 8px;
+    background: #fff;
+    transition: outline 0.1s;
+
+    &:focus-within:not(.input--error) {
+      outline: 1px solid var(--dd__gray--800);
+      color: var(--dd__gray--800);
+    }
+
+    &--disabled {
+      background: rgba(#e1e1e4, 0.25);
+    }
+
+    &--error {
+      outline: 1px solid var(--color--warning);
+    }
+
+    &__prefix {
+      flex: 1 1 auto;
+      display: flex;
+      align-items: center;
+    }
+
+    &__el {
+      box-sizing: border-box;
+      width: 100%;
+      height: 50px;
+      padding-block: var(--padding);
+      background: none;
+      border: 0;
+      outline: 0;
+      font-weight: 500;
+      font-size: 15px;
+      line-height: 1.2;
+      color: var(--dd__gray--800);
+      caret-color: var(--dd__blurple--400);
+
+      &::placeholder,
+      &:disabled {
+        color: var(--dd__gray--300);
+      }
+    }
+
+    &__clear {
+      --size: 16px;
+      all: unset;
+      display: none;
+      flex: 0 0 auto;
+      margin: 0 0 0 10px;
+      padding: 4px;
+      color: #fff;
+      cursor: pointer;
+
+      .input:focus-within & {
+        display: block;
+      }
+    }
+
+    &__clear-inner {
+      display: inline-grid;
+      place-items: center;
+      position: relative;
+      width: var(--size);
+      height: var(--size);
+      border-radius: 100%;
+      background: rgba(#b0b0ba, 0.5);
+      text-indent: -9999px;
+      font-size: 0.1em;
+
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        width: 2px;
+        height: 8px;
+        border-radius: 4px;
+        background: #fff;
+      }
+
+      &::before {
+        transform: rotate(45deg);
+      }
+      &::after {
+        transform: rotate(-45deg);
+      }
+    }
+
+    &__suffix:not(:empty) {
+      margin: 0 0 0 10px;
+    }
+  }
+
+  .message {
+    --color: var(--dd__gray--300);
+    position: relative;
+    margin: 12px 0 0;
+    padding: 0 0 0 14px;
+    color: var(--color);
+    font-size: 13px;
+    line-height: 1.6;
+
+    &::before {
+      --size: 3px;
+      content: '';
+      position: absolute;
+      left: 2.5px;
+      top: 8.5px;
+      width: var(--size);
+      height: var(--size);
+      background: var(--color);
+    }
+
+    &--error {
+      --color: var(--color--warning);
+    }
+  }
+</style>
