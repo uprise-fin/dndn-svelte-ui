56c6d0f6cdb3be9f6634313f02a33aaf676a92bc
hckang80 <hckang80@gmail.com>
2023-11-10 16:58:50 +0900

    chore: 임포트 방식 개선

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,3 +1,4 @@
 export * from './Button/type'
 export { default as Button } from './Button/Button.svelte'
 export { default as Modal } from './Modal/Modal.svelte'
+export { default as Input } from './Input/Input.svelte'
