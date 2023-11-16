adbdd9776ff15f3d8fe52354905e67cfb972ddeb
hckang80 <hckang80@gmail.com>
2023-11-16 14:11:58 +0900

    feat: 콘솔에 버전 출력

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,11 @@ import type { Preview } from '@storybook/svelte'
 import './style.css'
 import '../src/stories/assets/styles/index.scss'
 
+declare const __VERSION__: string
+
+const version = __VERSION__
+console.log(`%cVersion: v${version}`, 'color: lightgreen;')
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
