3d12824503af80860369409a85991363e9914fa9
hckang80 <hckang80@gmail.com>
2023-12-20 17:38:04 +0900

    feat: 글로벌 타입 배포방법 변경

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,1 @@
 export const size = ['small', 'medium', 'large'] as const
-export type Size = typeof size[number]
--- src/stories/Icon/Icon.svelte
+++ src/stories/Icon/Icon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { Size } from '$lib'
+  import type { Size } from '../../type'
   import SVG from './svg'
   import type { IconName } from './type'
 
