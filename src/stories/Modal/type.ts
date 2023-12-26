66c06567f2c94d4d75e7921e95e2ba996e250452
hckang80 <hckang80@gmail.com>
2023-12-26 19:56:27 +0900

    feat(modal): 타입 추가

--- src/stories/Modal/type.ts
+++ src/stories/Modal/type.ts
@@ -1,3 +1,15 @@
 export type Footer = {
   handler: () => void
 } & Record<string, unknown>
+
+export type Modal = {
+  open: boolean
+  width: string
+  toast: boolean
+  title: string
+  content: string
+  keepDialog: boolean
+  showCloseButton: boolean
+  isVerticalLayout: boolean
+  footers: Footer[]
+}
