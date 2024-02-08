6fe83b1c5e3f107a1e089ca52baa914bd9ca0e2a
hckang80 <hckang80@gmail.com>
2024-02-08 12:46:05 +0900

    feat: 아이콘 src 복사 기능 추가

--- src/stories/Icon/Icons.svelte
+++ src/stories/Icon/Icons.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
-  import { Icon } from '..'
+  import { Icon, Snackbar, showToast } from '..'
   import type { IconName } from './type'
 
   export let list: IconName[] = []
+
+  const copyContent = async (src: string) => {
+    try {
+      await navigator.clipboard.writeText(src)
+      showToast('This is the src to be copied')
+    } catch (err) {
+      console.error('Failed to copy: ', err)
+    }
+  }
 </script>
 
 <ul class="list">
   {#each list as src}
     <li class="list__item">
-      <Icon {src} />
-      <span class="list__name">
-        {src.split('/').pop()}
-      </span>
+      <button class="list__button" on:click={() => copyContent(src)}>
+        <Icon {src} />
+        <span class="list__name">
+          {src.split('/').pop()}
+        </span>
+      </button>
     </li>
   {/each}
 </ul>
+
+<Snackbar />
 
 <style lang="scss">
   .list {
@@ -27,12 +40,23 @@
     padding: 0;
 
     &__item {
+      display: contents;
+    }
+
+    &__button {
       display: flex;
       gap: 10px;
       flex-flow: column;
       justify-content: center;
       align-items: center;
       border: 1px solid var(--text-primary);
+      cursor: pointer;
+      background: var(--background);
+
+      &:hover :global(.icon) {
+        transition: transform 0.3s;
+        transform: scale(2);
+      }
     }
 
     &__name {
