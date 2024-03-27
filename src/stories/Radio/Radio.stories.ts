af2a81449bce08460b115d09d86bb6b1741ea500
hckang80 <hckang80@gmail.com>
2024-03-27 17:54:32 +0900

    feat(radio): update story

--- src/stories/Radio/Radio.stories.ts
+++ src/stories/Radio/Radio.stories.ts
@@ -13,7 +13,7 @@ const meta = {
       },
     },
     checked: {
-      description: 'options 아이템의 value에 해당합니다. 초기 체크된 값을 변경할 수 있습니다.',
+      description: '기본 값을 임의로 변경할 수 있습니다. options 아이템의 value에 해당합니다.',
       table: {
         type: { summary: 'string | number' },
       },
@@ -30,33 +30,47 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Basic: Story = {
+export const basic: Story = {
   args: {
     options: [
       {
-        value: 'green',
+        value: 6,
         label: '6등급',
       },
       {
-        value: 'red',
+        value: 5,
         label: '5등급',
       },
       {
-        value: 'blue',
+        value: 4,
         label: '4등급',
       },
       {
-        value: 'green2',
+        value: 3,
         label: '3등급',
       },
       {
-        value: 'red2',
+        value: 2,
         label: '2등급',
       },
       {
-        value: 'blue2',
+        value: 1,
         label: '1등급',
       },
     ],
+  },
+}
+
+export const verticalLayout = {
+  args: {
+    ...basic.args,
+    isVerticalLayout: true,
+  },
+}
+
+export const block = {
+  args: {
+    ...basic.args,
+    isBlock: true,
   },
 }
